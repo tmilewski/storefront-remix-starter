831633b70b8c88af06833c834a2008dbf7c41030
Tom Milewski <me@tm.codes>
2022-09-04 10:05:21 -0400

    chore(StockLevelLabel): refactor

--- app/components/products/StockLevelLabel.tsx
+++ app/components/products/StockLevelLabel.tsx
@@ -1,25 +1,27 @@
-export type StockLevel = 'IN_STOCK' | 'OUT_OF_STOCK' | 'LOW_STOCK';
+export enum StockLevel {
+  Default = 'DEFAULT',
+  InStock = 'IN_STOCK',
+  LowStock = 'LOW_STOCK',
+  OutOfStock = 'OUT_OF_STOCK',
+}
 
-export function StockLevelLabel({stockLevel}: { stockLevel?: string; }) {
-    let stockLevelLabel = '';
-    let badgeClasses = 'bg-gray-100 text-gray-800';
-    switch (stockLevel as StockLevel) {
-        case 'IN_STOCK':
-            stockLevelLabel = 'In stock';
-            badgeClasses = 'bg-green-100 text-green-800'
-            break;
-        case 'OUT_OF_STOCK':
-            stockLevelLabel = 'Out of stock';
-            badgeClasses = 'bg-red-100 text-red-800'
-            break;
-        case 'LOW_STOCK':
-            stockLevelLabel = 'Low stock';
-            badgeClasses = 'bg-yellow-100 text-yellow-800'
-            break;
-    }
+export type StockLevelLabelProps = { stockLevel?: StockLevel }
+
+type StockDetails = { label: string; classes: string }
+
+const stockLevel = new Map<StockLevel, StockDetails>([
+    [StockLevel.Default, { label: '', classes: 'bg-gray-100 text-gray-800'] },
+    [StockLevel.InStock, { label: 'In stock', classes: 'bg-green-100 text-green-800'] },
+    [StockLevel.LowStock, { label: 'Low stock', classes: 'bg-yellow-100 text-yellow-800'] },
+    [StockLevel.OutOfStock, { label: 'Out of stock', classes: 'bg-red-100 text-red-800'] },
+])
+
+export function StockLevelLabel({ stockLevel = StockLevel.Default }: StockLevelLabelProps) {
+    const details = stockLevel.get(stockLevel)
+    
     return (
-        <span className={'inline-flex items-center px-2 py-0.5 rounded text-xs font-medium ' + badgeClasses}>
-          {stockLevelLabel}
+        <span className={'inline-flex items-center px-2 py-0.5 rounded text-xs font-medium ' + details.classes}>
+          {details.label}
         </span>
     )
 }
